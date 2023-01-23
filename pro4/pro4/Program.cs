131d10895e54de220698fb32ecdf2ba6f692310b
jeweljose22 <117150116+jeweljose22@users.noreply.github.com>
2023-01-23 17:53:26 +0530

    Add files via upload

---
+++ pro4/pro4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("enter your dob in (dd/mm/yyyy");
+double[] fare = new double[5];
+for (int i = 1;i < 5; i++)
+{
+    if( i % 2 == 0)
+    {
+        Console.WriteLine("enter at even pos");
+        fare[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    else
+    {
+        Console.WriteLine("enter at odd pos");
+        fare[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+foreach (int item in fare)
+    Console.WriteLine(item);
